e07da914ada045b06e36979f6b05958ed59a79bb
annette Bwemere <bwemereannette@gmail.com>
2022-10-22 09:18:18 +0200

    feat: add function errorhanler and normalizePort for post a valide port and lister error in server

--- backend/app.js
+++ backend/app.js
@@ -3,4 +3,27 @@ const express = require('express');
 const app = express();
 
 
+app.use((req, res, next) => {
+    console.log('Requête reçue !');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+
+
+app.use((req, res, next) => {
+    res.json({ message: 'Votre réquete à ete bien réussie' });
+    next()
+});
+
+
+app.use((req, res, next) => {
+    console.log('Fait Avec Annette');
+})
+
+
 module.exports = app;
